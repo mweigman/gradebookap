12ac3f9b35ce0b228c3a9858ea8698c059debb99
Melissa Weigman <melissa_weigman@outlook.com>
2020-12-02 21:46:39 -0600

    second assignment

--- gradebook.js
+++ gradebook.js
@@ -1,7 +1,11 @@
+var studentsArry = [];
+
 $(document).ready(function () {
 
     //events
     $("#submit").click(results);
+    $("#sortName").click(sortName);
+    $("#sortPercent").click(sortPercentage);
 
     //functions
     function results(event) {
@@ -12,7 +16,8 @@ $(document).ready(function () {
                 firstName: "",
                 lastName: "",
                 pointsEarned: 0,
-                pointsPossible: 0
+                pointsPossible: 0,
+                calculation: 0
             };
 
         myPoints.firstName = $("#firstName").val();
@@ -24,6 +29,8 @@ $(document).ready(function () {
 
         $("#resultsOutput").text(` ${myPoints.firstName} ${myPoints.lastName} ${calculation.toFixed()}%`);
 
+        myPoints.calculation = calculation;
+
         if (calculation < 80) {
             $("#letterOutput").text("C");
         } else if (calculation < 90) {
@@ -31,5 +38,65 @@ $(document).ready(function () {
         } else {
             $("#letterOutput").text("A");
         }
+
+        studentsArry.push(myPoints);
+
+        var output = "";
+
+        for (var studentInfo of studentsArry) {
+
+            output += studentInfo.firstName;
+
+        }
+
+        $("#letterOutput").text(output);
+
     }
+
+    function sortName() {
+        studentsArry.sort(function (a, b) {
+            if (a.firstName < b.firstName) {
+                return -1;
+            } else if (a.firstName < b.firstName) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        var output = "";
+
+        for (var studentInfo of studentsArry) {
+
+            output += studentInfo.firstName;
+
+        }
+
+        $("#resultsOutput").text(output);
+
+    }
+
+    function sortPercentage() {
+        studentsArry.sort(function (a, b) {
+            if (a.calculation < b.calculation) {
+                return -1;
+            } else if (a.calculation < b.calculation) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        var output = "";
+
+        for (var studentInfo of studentsArry) {
+
+            output += studentInfo.calculation;
+
+        }
+
+        $("#letterOutput").text(output);
+    }
+
+
 });
